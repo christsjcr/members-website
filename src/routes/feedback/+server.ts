7792d4847078c64b94495106622f2fe97585bcca
Miles Watson <contact@mileswatson.net>
2022-11-28 22:08:19 +0000

    Added basic auth + email API.

---
+++ src/routes/feedback/+server.ts
@@ -0,0 +1,21 @@
+import type { RequestHandler } from "./$types"
+
+const clientId = import.meta.env.VITE_CLIENT_ID ?? process.env.CLIENT_ID;
+const domain = import.meta.env.VITE_DOMAIN ?? "https://members.thejcr.co.uk";
+
+const authURL = 'https://accounts.google.com/o/oauth2/v2/auth?hd=cam.ac.uk';
+const redirectURL = `${domain}/feedback/callback`;
+
+
+const POST: RequestHandler = async (event) => {
+	console.log(event);
+	const state = event.url.searchParams.get('content') ?? "";
+	return new Response("", {
+		status: 302,
+		headers: {
+			location: `${authURL}&client_id=${clientId}&state=${encodeURIComponent(state)}&scope=profile email openid&response_type=code&redirect_uri=${encodeURIComponent(redirectURL)}`,
+		}
+	});
+};
+
+export { POST };
---
+++ src/routes/feedback/callback/+server.ts
@@ -0,0 +1,100 @@
+import type { RequestHandler } from "./$types"
+import nodemailer from "nodemailer";
+
+const clientId = import.meta.env.VITE_CLIENT_ID ?? process.env.CLIENT_ID;
+const secret = import.meta.env.VITE_CLIENT_SECRET ?? process.env.CLIENT_SECRET;
+const domain = import.meta.env.VITE_DOMAIN ?? "https://thejcr.co.uk";
+const publicDomain = import.meta.env.VITE_PUBLIC_DOMAIN ?? "https://thejcr.co.uk";
+const feedbackEmailAddress = import.meta.env.VITE_FEEDBACK_EMAIL_ADDRESS ?? process.env.FEEDBACK_EMAIL_ADDRESS;
+const feedbackEmailPassword = import.meta.env.VITE_FEEDBACK_EMAIL_PASSWORD ?? process.env.FEEDBACK_EMAIL_PASSWORD;
+
+const tokenURL = 'https://oauth2.googleapis.com/token';
+const userURL = 'https://openidconnect.googleapis.com/v1/userinfo';
+const redirectURL = `${domain}/feedback/callback`;
+
+async function getAccessToken(code: string) {
+	const response = await fetch(tokenURL, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+		body: JSON.stringify({
+			client_id: clientId,
+			client_secret: secret,
+			redirect_uri: redirectURL,
+			grant_type: 'authorization_code',
+			code
+		})
+	});
+	const json = await response.json();
+	return json.access_token;
+}
+
+async function getUser(accessToken: string) {
+	const response = await fetch(userURL, {
+		headers: {
+			Accept: 'application/json',
+			Authorization: `Bearer ${accessToken}`
+		}
+	});
+	return await response.json();
+}
+
+function redirect(location: string): Response {
+	return new Response("", {
+		status: 302,
+		headers: {
+			location
+		}
+	})
+}
+
+function error(message: string): Response {
+	return redirect(`${publicDomain}/feedback/error?message=${encodeURIComponent(message)}`)
+}
+
+async function send(message: string) {
+	const transport = nodemailer.createTransport({
+		host: "mail.postale.io",
+		port: 465,
+		auth: {
+			user: feedbackEmailAddress,
+			pass: feedbackEmailPassword,
+		},
+	});
+	const mailOptions = {
+		from: feedbackEmailAddress,
+		to: feedbackEmailAddress,
+		subject: "Feedback Test",
+		text: message,
+	};
+	await transport.sendMail(mailOptions);
+}
+
+const GET: RequestHandler = async (event) => {
+	const code = event.url.searchParams.get('code');
+
+	try {
+		const accessToken = await getAccessToken(code ?? "");
+	
+		const user = await getUser(accessToken);
+
+		console.log(user);
+
+		if (user == null) {
+			return error("Authentication failed!");
+		}
+	} catch(e) {
+		console.error(e);
+		return error("Authentication failed!");
+	}
+
+	try {
+		await send("Test Message");
+	} catch (e) {
+		console.error(e);
+		return error("Failed to send email!");
+	}
+
+	return redirect(`${publicDomain}/feedback/success`);
+};
+
+export { GET };
