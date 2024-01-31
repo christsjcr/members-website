38dbeda35d000bcabb4d176d398c860906ade42f
Rachel Leona <34417799+rachleona@users.noreply.github.com>
2024-01-31 12:04:13 +0000

    Updated people.ts
    Still missing vice president and ents

--- src/routes/api/feedback/people.ts
+++ src/routes/api/feedback/people.ts
@@ -1,33 +1,32 @@
 // Committee members that can reveal the identities of anonymous form submissions,
 // all of which will be alerted if such a reveal occurs
 const valid_revealers = {
-    ov235: "president",
-    cl888: "vicepresident",
-    fpk24: "treasurer",
+    sim36: "president",
+    // "vicepresident"
+    bjr43: "treasurer",
     cd796: "secretary",
-    jl2323: "welfare-m",
-    om377: "welfare-f",
-    mtw43: "webmaster",
+    za325: "welfare-m",
+    cl986: "welfare-f",
+    wyrt2: "webmaster",
 };
 
 // Committee members that can respond to form submissions
 const valid_responders = {
     ...valid_revealers,
-    np578: "ents",
-    qd227: "firstyearrep",
-    kb786: "classact",
-    aa2315: "edo",
-    ams315: "womens",
-    jyt33: "intl",
-    // "lgbt",
-    yld21: "access",
-    mr936: "facilities",
-    red50: "green",
+    // "ents"
+    fm597: "firstyearrep",
+    mc2374: "classact",
+    mda54: "edo",
+    eh721: "womens",
+    ap2330: "intl",
+    dw644: "lgbt",
+    gv310: "access",
+    ab2899: "facilities",
+    mykk2: "green",
 };
 
 // Committee members that can be selected under the "Share With: Specific officers only" option
 const valid_recipients = Array.from(new Set(Object.values(valid_responders)));
-valid_recipients.push("lgbt");
 
 const exec_recipients = [
     "president",
